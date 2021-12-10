4ae993b88c3fd207a953ab5c47f18e5584286639
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2021-12-10 16:45:46 +0100

    first commit

---
+++ TP1-OCP/src/SpaceResource.java
@@ -0,0 +1,21 @@
+
+public class SpaceResource implements Resource
+{
+    @Override
+    public int findFree()
+    {
+        return 0;
+    }
+
+    @Override
+    public void markBusy(int resourceId)
+    {
+
+    }
+
+    @Override
+    public void markFree(int resourceId)
+    {
+
+    }
+}
---
+++ TP1-OCP/src/TimeResource.java
@@ -0,0 +1,21 @@
+
+public class TimeResource implements Resource
+{
+    @Override
+    public int findFree()
+    {
+        return 0;
+    }
+
+    @Override
+    public void markBusy(int resourceId)
+    {
+
+    }
+
+    @Override
+    public void markFree(int resourceId)
+    {
+
+    }
+}
